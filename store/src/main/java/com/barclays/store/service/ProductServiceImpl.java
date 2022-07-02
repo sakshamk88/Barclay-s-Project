70399ce55ee1ce5dd56b1f8cd62a7e6e43ad163e
sakshamk88 <sakshamk88@gmail.com>
2022-07-03 02:01:42 +0530

    model mapper added

--- store/src/main/java/com/barclays/store/service/ProductServiceImpl.java
+++ store/src/main/java/com/barclays/store/service/ProductServiceImpl.java
@@ -1,5 +1,40 @@
 package com.barclays.store.service;
 
-public class ProductServiceImpl {
+import java.util.List;
+import java.util.Optional;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.barclays.store.dto.ProductDTO;
+import com.barclays.store.entity.Product;
+import com.barclays.store.repository.ProductRepository;
+
+@Service
+public class ProductServiceImpl implements ProductService{
+
+	@Autowired
+	ProductRepository productRepository;
+	
+	@Autowired
+	ModelMapper modelMapper;
+	
+	@Override
+	public ProductDTO getProductDetails(Integer productId) {
+		// TODO Auto-generated method stub
+		Optional<Product> op = productRepository.findById(productId);
+		ProductDTO pDto = null;
+		if (op.isPresent()){
+			 pDto = modelMapper.map(op.get(), ProductDTO.class);
+		}
+		return pDto;
+	}
+
+	@Override
+	public List<ProductDTO> getAllProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
 }
--- store/src/main/java/com/barclays/store/service/UserServiceImpl.java
+++ store/src/main/java/com/barclays/store/service/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package com.barclays.store.service;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class UserServiceImpl {
 
 }
