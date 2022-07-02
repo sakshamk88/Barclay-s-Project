84f5f29650d39019ebe937b09f8b0a85d0b3a2b4
Tarun_Bhambhani <tarunbambani@gmail.com>
2022-07-03 03:12:18 +0530

    Updated cartService

--- store/src/main/java/com/barclays/store/repository/CartRepository.java
+++ store/src/main/java/com/barclays/store/repository/CartRepository.java
@@ -1,10 +1,16 @@
 package com.barclays.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.barclays.store.entity.Cart;
 
+import java.util.List;
+
 public interface CartRepository extends JpaRepository<Cart, Integer> {
+
+    @Query(value = "Select * from cart where cart_i:cartId",nativeQuery = true)
+    public List<Cart> findByCartId(Integer cartId);
 
 }
