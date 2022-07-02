81d59350e24699d84291a31ff87525e311b05d81
sakshamk88 <sakshamk88@gmail.com>
2022-07-03 03:39:14 +0530

    merge

--- store/src/main/java/com/barclays/store/dto/CustomerDTO.java
+++ store/src/main/java/com/barclays/store/dto/CustomerDTO.java
@@ -10,4 +10,30 @@ public class CustomerDTO {
 
     private String phoneNumber;
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+    
+    
+
 }
