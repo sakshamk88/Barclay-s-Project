c5a583403decf64ce5f5c505022b8358fbca1c42
Tarun_Bhambhani <tarunbambani@gmail.com>
2022-07-03 02:38:48 +0530

    Updated CustomerService

--- store/src/main/java/com/barclays/store/repository/CustomerRepository.java
+++ store/src/main/java/com/barclays/store/repository/CustomerRepository.java
@@ -1,10 +1,22 @@
 package com.barclays.store.repository;
 
+import com.barclays.store.dto.CustomerDTO;
+import com.barclays.store.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import com.barclays.store.entity.Customer;
 
-public interface CustomerRepository extends JpaRepository<Customer, String> {
+import java.util.List;
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+
+    @Query(value="Select * from customer where email_id=:password",nativeQuery = true)
+    public Optional<CustomerDTO> findByEmailId(String password);
+
+    @Query(value = "Select * from cart where cart_id=:cart_id",nativeQuery = true)
+    public List<Cart> findByCartId(Integer cartId);
 
 }
